d2979f7d14ed4656754d4e96d08fb09ed2a6a23a
Devarsh D Sanghvi <devarshsanghvi.ds@gmail.com>
2021-04-05 15:44:32 +0530

    Made get-ip

---
+++ get-ip.py
@@ -0,0 +1,18 @@
+import flask
+import threading
+app = flask.Flask(__name__)
+
+def trd():
+    global app
+    
+    app.run(host="0.0.0.0", port=8080)
+    print('thread')
+@app.route("/")
+def index():
+
+    ip_address = flask.request.remote_addr
+    return ip_address
+    
+td = threading.Thread(target = trd)
+td.start()
+#app.run(host="0.0.0.0", port=8080)
